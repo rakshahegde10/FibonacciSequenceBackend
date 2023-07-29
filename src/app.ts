8f18ef2d7bd168d21f615aa2064d3ccfa42eb16a
rakshahegde10 <rakshahegde10@gmail.com>
2023-07-29 02:42:02 -0400

    added cors for react

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import fibonacciRoutes from './routes/fibonacci.routes';
+import cors from 'cors';
+import helmet from 'helmet';
 
 export const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({ origin: '*' })); // Allow requests from any origin
+
 app.use(bodyParser.json()); // Use the bodyParser middleware to parse JSON data
 
 app.use('/api/fibonacci', fibonacciRoutes); //Fibonacci router
+
+app.use(helmet());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -2,13 +2,19 @@ import request from 'supertest';
 import express from 'express';
 import bodyParser from 'body-parser';
 import fibonacciRoutes from '../routes/fibonacci.routes';
+import cors from 'cors';
+import helmet from 'helmet';
 
 export const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({ origin: '*' })); // Allow requests from any origin
+
 app.use(bodyParser.json()); // Use the bodyParser middleware to parse JSON data
 
 app.use('/api/fibonacci', fibonacciRoutes); //Fibonacci router
+
+app.use(helmet());
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
